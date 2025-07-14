63909032a7a36b96eecae15068a1445fb02e2d5f
Phuntsho <pwangmo2019@gmail.com>
2025-07-14 23:45:36 +0600

    Adds filter_list function

--- function.js
+++ function.js
@@ -273,7 +273,7 @@ console.log(repeatStr (5,"t"));
 function otherAngle(a, b) {
   return 180 - (a+b);
 }
-console.log(otherAngle(60,60);
+console.log(otherAngle(60,60));
 
 // Create a function which answers the question "Are you playing banjo?".
 // If your name starts with the letter "R" or lower case "r", you are playing banjo!
@@ -411,3 +411,16 @@ function hello(name) {
 console.log(hello("john"));
 console.log(hello(""));         // "Hello, World!"
 console.log(hello());           // "Hello, World!"
+
+// In this kata you will create a function that takes a list of non-negative 
+// integers and strings and returns a new list with the strings filtered out.
+
+// Example
+// filter_list([1,2,'a','b']) == [1,2]
+// filter_list([1,'a','b',0,15]) == [1,0,15]
+// filter_list([1,2,'aasf','1','123',123]) == [1,2,123]
+
+function filter_list(l) {
+   return l.filter(item => typeof item === 'number');
+}
+console.log(filter_list([1,3,4,5,'a','b','c']));
